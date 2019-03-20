e1658312e66671b3ba95aa8ec922f53bec6e67f6
raunol <rauno.lohmus@gmail.com>
2019-03-20 20:25:19 +0200

    first iteration completed + test data

--- build.sbt
+++ build.sbt
@@ -3,6 +3,9 @@ name := "SBI"
 version := "0.1"
 
 scalaVersion := "2.12.8"
+val sparkVersion = "2.4.0"
 
-
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.0"
+libraryDependencies ++= Seq(
+"org.apache.spark" %% "spark-core" % sparkVersion,
+"org.apache.spark" %% "spark-sql" % sparkVersion
+)
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,11 +1,51 @@
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 object main {
+
+
+
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("SBI").setMaster("local[2]").set("spark.executor.memory", "1g")
     val sc = new SparkContext(conf)
 
-    val hyperGraphRdd = sc.textFile("C:\\Users\\raunol\\Desktop\\flights.csv").map(line => line.split(" "))
+    var to_explore = Array[String]()
+    val sqlContext = new SQLContext(sc)
+    sc.setLogLevel("Error")
+
+
+    val hyperGraphRdd = sc.textFile("path").map(line => line.split(" ")).zipWithIndex().map { case (x, y) => (y, x) }
+    val maxTransactions = hyperGraphRdd.collect().length
+    val itemList = hyperGraphRdd.flatMap(_._2.toList).map(x => (x, 1)).reduceByKey((x, y) => x + y).collect().sortBy { case (k, v) => (v, k) }
+
+   to_explore = to_explore ++ firstIterationFindLastITem(hyperGraphRdd, maxTransactions, itemList)
+
+    var table = Array[Array[String]]()
+     for{
+       x <- to_explore
+          y <- itemList
+     }yield Array(x, y._1)
+
+    for(x <- to_explore; y <- itemList if (!x.equals(y._1))) yield Array(x, y._1)
+
 
   }
+
+  def firstIterationFindLastITem(hyperGraphRdd: org.apache.spark.rdd.RDD[(Long, Array[String])], maxTransactions: Int, itemList: Array[(String, Int)]): Array[String] = {
+    var result = Array[String]()
+    var items = hyperGraphRdd.filter(g => g._2.contains(itemList(0)._1)).collect()
+    var usedIndexes = items.map(x => x._1)
+    for (elem <- itemList) {
+      val newItems = hyperGraphRdd.filter(g => !usedIndexes.contains(g._1) && g._2.contains(elem._1)).collect()
+      items = items.union(newItems)
+      usedIndexes = usedIndexes ++ newItems.map(x => x._1)
+      if (items.length == maxTransactions) {
+        result = result ++ Array(elem._1)
+      }
+    }
+    result
+  }
+
+
 }
